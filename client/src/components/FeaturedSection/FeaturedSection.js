52f3412dfd57a4d1a31e067623e7f3dbb967d59e
Amir <amirj.castaneda@gmail.com>
2022-03-19 20:52:31 +0800

    Used flex-start instead of start

--- client/src/components/FeaturedSection/FeaturedSection.js
+++ client/src/components/FeaturedSection/FeaturedSection.js
@@ -1,5 +1,5 @@
 import Button from '../Button/Button';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 
 export default function CarouselSection() {
